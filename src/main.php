48a109d1362b3046764fa048ffe26de8b739f460
Valentin Dibbern PC <valentin.dibbern@icloud.com>
2024-12-15 21:46:45 +0100

    further further fixes

--- src/main.php
+++ src/main.php
@@ -1,47 +1,49 @@
 <?php
-// Initialize variables for total price and form data
-$totalPrice = 0;
-$cores = null;
-$ram = null;
-$ssd = null;
-
 // Check if form data is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get selected values from the form
-    $cores = isset($_POST['cores']) ? $_POST['cores'] : null;
-    $ram = isset($_POST['ram']) ? $_POST['ram'] : null;
-    $ssd = isset($_POST['ssd']) ? $_POST['ssd'] : null;
 
-    // Price calculation for cores
-    switch ($cores) {
-        case '1': $totalPrice += 5; break;
-        case '2': $totalPrice += 10; break;
-        case '4': $totalPrice += 18; break;
-        case '8': $totalPrice += 30; break;
-        case '16': $totalPrice += 45; break;
-    }
+    // init var and const
+    $totalPrice = 0;
 
-    // Price calculation for RAM
-    switch ($ram) {
-        case '512': $totalPrice += 5; break;
-        case '1024': $totalPrice += 10; break;
-        case '2048': $totalPrice += 20; break;
-        case '4096': $totalPrice += 40; break;
-        case '8192': $totalPrice += 80; break;
-        case '16384': $totalPrice += 160; break;
-        case '32768': $totalPrice += 320; break;
-    }
+    $coresPrice = [
+        '1' => 5,
+        '2' => 10,
+        '4' => 20,
+        '8' => 30,
+        '16' => 45
+    ];
 
-    // Price calculation for SSD
-    switch ($ssd) {
-        case '10': $totalPrice += 5; break;
-        case '20': $totalPrice += 10; break;
-        case '40': $totalPrice += 20; break;
-        case '80': $totalPrice += 40; break;
-        case '240': $totalPrice += 120; break;
-        case '500': $totalPrice += 250; break;
-        case '1000': $totalPrice += 500; break;
-    }
+    $ramPrice = [
+         '512' => 5,
+         '1024' => 10,
+         '2048' => 20,
+         '4096' => 40,
+         '8192' => 80,
+         '16384' => 160,
+         '32768' => 320
+    ];
+
+    $ssdPrice = [
+         '10' => 5,
+         '20' => 10,
+         '40' => 20,
+         '80' => 40,
+         '240' => 120
+    ];
+
+   if (isset($_POST['cores']) && isset($coresPrice[$_POST['cores']])) {
+       $totalPrice += $coresPrice[$_POST['cores']];
+   }
+
+       // Preis für RAM berechnen
+   if (isset($_POST['ram']) && isset($ramPrice[$_POST['ram']])) {
+       $totalPrice += $ramPrice[$_POST['ram']];
+   }
+
+       // Preis für SSD berechnen
+   if (isset($_POST['ssd']) && isset($ssdPrice[$_POST['ssd']])) {
+       $totalPrice += $ssdPrice[$_POST['ssd']];
+   }
 
     // Redirect to the confirmation page with the total price
     header("Location: price_page.php?totalPrice=" . urlencode($totalPrice));
