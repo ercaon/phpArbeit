f2b1d9233d6ae290620c7c9333450b0a586ae6f6
Valentin Dibbern PC <valentin.dibbern@icloud.com>
2024-12-15 22:17:07 +0100

    developed further

--- src/main.php
+++ src/main.php
@@ -1,52 +1,50 @@
 <?php
-// Check if form data is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if form data is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // init var and const
-    $totalPrice = 0;
+        // init var and const
+        $totalPrice = 0;
 
-    $coresPrice = [
-        '1' => 5,
-        '2' => 10,
-        '4' => 20,
-        '8' => 30,
-        '16' => 45
-    ];
+        $coresPrice = [
+            '1' => 5,
+            '2' => 10,
+            '4' => 18,
+            '8' => 30,
+            '16' => 45
+        ];
 
-    $ramPrice = [
-         '512' => 5,
-         '1024' => 10,
-         '2048' => 20,
-         '4096' => 40,
-         '8192' => 80,
-         '16384' => 160,
-         '32768' => 320
-    ];
+        $ramPrice = [
+            '512' => 5,
+            '1024' => 10,
+            '2048' => 20,
+            '4096' => 40,
+            '8192' => 80,
+            '16384' => 160,
+            '32768' => 320
+        ];
 
-    $ssdPrice = [
-         '10' => 5,
-         '20' => 10,
-         '40' => 20,
-         '80' => 40,
-         '240' => 120
-    ];
+        $ssdPrice = [
+            '10' => 5,
+            '20' => 10,
+            '40' => 20,
+            '80' => 40,
+            '240' => 120,
+            '500' => 250,
+            '1000' => 500
+        ];
 
-   if (isset($_POST['cores']) && isset($coresPrice[$_POST['cores']])) {
-       $totalPrice += $coresPrice[$_POST['cores']];
-   }
+        if (isset($_POST['cores']) && isset($coresPrice[$_POST['cores']])) {
 
-       // Preis für RAM berechnen
-   if (isset($_POST['ram']) && isset($ramPrice[$_POST['ram']])) {
-       $totalPrice += $ramPrice[$_POST['ram']];
-   }
+        }
 
-       // Preis für SSD berechnen
-   if (isset($_POST['ssd']) && isset($ssdPrice[$_POST['ssd']])) {
-       $totalPrice += $ssdPrice[$_POST['ssd']];
-   }
+        // Preis für RAM berechnen
+        if (isset($_POST['ram']) && isset($ramPrice[$_POST['ram']])) {
+            $totalPrice += $ramPrice[$_POST['ram']];
+        }
 
-    // Redirect to the confirmation page with the total price
-    header("Location: price_page.php?totalPrice=" . urlencode($totalPrice));
-    exit();
-}
+        // Preis für SSD berechnen
+        if (isset($_POST['ssd']) && isset($ssdPrice[$_POST['ssd']])) {
+            $totalPrice += $ssdPrice[$_POST['ssd']];
+        }
+    }
 ?>
